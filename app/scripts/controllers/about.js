f0f576502857e3455401029b8603d9bc7feb006c
masonfox <masonfox22@gmail.com>
2015-11-20 14:22:18 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coopwebclassgithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the coopwebclassgithubioApp
+ */
+angular.module('coopwebclassgithubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
